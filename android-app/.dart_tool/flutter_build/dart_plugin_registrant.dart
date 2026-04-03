d5e4faa5ffdedf426238df02e0a31c2b0e7f8b71
Shahd Emad <134103174+shahdcode@users.noreply.github.com>
2026-04-03 19:55:15 +0200

    trying

--- android-app/.dart_tool/flutter_build/dart_plugin_registrant.dart
+++ android-app/.dart_tool/flutter_build/dart_plugin_registrant.dart
@@ -6,14 +6,14 @@
 // @dart = 3.7
 
 import 'dart:io'; // flutter_ignore: dart_io_import.
-import 'package:path_provider_android/path_provider_android.dart';
-import 'package:video_player_android/video_player_android.dart';
-import 'package:path_provider_foundation/path_provider_foundation.dart';
-import 'package:video_player_avfoundation/video_player_avfoundation.dart';
-import 'package:path_provider_linux/path_provider_linux.dart';
-import 'package:path_provider_foundation/path_provider_foundation.dart';
-import 'package:video_player_avfoundation/video_player_avfoundation.dart';
-import 'package:path_provider_windows/path_provider_windows.dart';
+import 'package:path_provider_android/path_provider_android.dart' as path_provider_android;
+import 'package:video_player_android/video_player_android.dart' as video_player_android;
+import 'package:path_provider_foundation/path_provider_foundation.dart' as path_provider_foundation;
+import 'package:video_player_avfoundation/video_player_avfoundation.dart' as video_player_avfoundation;
+import 'package:path_provider_linux/path_provider_linux.dart' as path_provider_linux;
+import 'package:path_provider_foundation/path_provider_foundation.dart' as path_provider_foundation;
+import 'package:video_player_avfoundation/video_player_avfoundation.dart' as video_player_avfoundation;
+import 'package:path_provider_windows/path_provider_windows.dart' as path_provider_windows;
 
 @pragma('vm:entry-point')
 class _PluginRegistrant {
@@ -22,7 +22,7 @@ class _PluginRegistrant {
   static void register() {
     if (Platform.isAndroid) {
       try {
-        PathProviderAndroid.registerWith();
+        path_provider_android.PathProviderAndroid.registerWith();
       } catch (err) {
         print(
           '`path_provider_android` threw an error: $err. '
@@ -31,7 +31,7 @@ class _PluginRegistrant {
       }
 
       try {
-        AndroidVideoPlayer.registerWith();
+        video_player_android.AndroidVideoPlayer.registerWith();
       } catch (err) {
         print(
           '`video_player_android` threw an error: $err. '
@@ -41,7 +41,7 @@ class _PluginRegistrant {
 
     } else if (Platform.isIOS) {
       try {
-        PathProviderFoundation.registerWith();
+        path_provider_foundation.PathProviderFoundation.registerWith();
       } catch (err) {
         print(
           '`path_provider_foundation` threw an error: $err. '
@@ -50,7 +50,7 @@ class _PluginRegistrant {
       }
 
       try {
-        AVFoundationVideoPlayer.registerWith();
+        video_player_avfoundation.AVFoundationVideoPlayer.registerWith();
       } catch (err) {
         print(
           '`video_player_avfoundation` threw an error: $err. '
@@ -60,7 +60,7 @@ class _PluginRegistrant {
 
     } else if (Platform.isLinux) {
       try {
-        PathProviderLinux.registerWith();
+        path_provider_linux.PathProviderLinux.registerWith();
       } catch (err) {
         print(
           '`path_provider_linux` threw an error: $err. '
@@ -70,7 +70,7 @@ class _PluginRegistrant {
 
     } else if (Platform.isMacOS) {
       try {
-        PathProviderFoundation.registerWith();
+        path_provider_foundation.PathProviderFoundation.registerWith();
       } catch (err) {
         print(
           '`path_provider_foundation` threw an error: $err. '
@@ -79,7 +79,7 @@ class _PluginRegistrant {
       }
 
       try {
-        AVFoundationVideoPlayer.registerWith();
+        video_player_avfoundation.AVFoundationVideoPlayer.registerWith();
       } catch (err) {
         print(
           '`video_player_avfoundation` threw an error: $err. '
@@ -89,7 +89,7 @@ class _PluginRegistrant {
 
     } else if (Platform.isWindows) {
       try {
-        PathProviderWindows.registerWith();
+        path_provider_windows.PathProviderWindows.registerWith();
       } catch (err) {
         print(
           '`path_provider_windows` threw an error: $err. '
